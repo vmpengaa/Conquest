f8bbf070111ff2ee9e7335d811784f69cb72e5c9
ademri <amirdemri1@gmail.com>
2016-04-02 04:11:06 +0200

    Added files via upload

---
+++ Conquest/src/gui/TileGrid.java
@@ -0,0 +1,45 @@
+package data;
+
+import static helpers.Artist.*;
+
+
+public class TileGrid {
+	public Tile[][] map;
+	
+	public TileGrid() {
+		map = new Tile[32][21];
+		for (int i = 0; i < map.length; i++){
+			for (int j = 0; j < map[i].length; j++) {
+				map[i][j] = new Tile(i*32, j*32, 32, 32, TileType.Grass);
+				
+			}
+		}
+	}
+	
+	public TileGrid(int[][] newMap){
+		map = new Tile[32][21];
+		for (int i = 0; i < map.length; i++){
+			for (int j = 0; j < map[i].length; j++){
+				if ( newMap[j][i] == 0)
+					map[i][j] = new Tile(i*32, j*32, 32, 32, TileType.Grass);
+				else {
+					if ( newMap[j][i] == 1)
+						map[i][j] = new Tile(i*32, j*32, 32, 32, TileType.Dirt);
+					else
+					map[i][j] = new Tile(i*32, j*32, 32, 32, TileType.Water);
+				}
+			}
+		}
+	}
+		
+		
+	public void Draw(){
+		for(int i = 0; i < map.length; i++){
+			for (int j = 0; j < map[i].length; j++){
+				Tile t = map[i][j];
+				DrawHexagoneTex(t.getTexture(), t.getX(), t.getY(), t.getWidth(), t.getHeight());
+			}
+		}
+	}
+	
+}
